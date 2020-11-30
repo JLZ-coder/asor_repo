12c8e71fd33a9fc9bcfbfad252d42c7c0b781c1d
JLZ-coder <60971493+JLZ-coder@users.noreply.github.com>
2020-11-30 18:44:39 +0100

    Create ejercicio5.c

---
+++ practica2.1/ejercicio5.c
@@ -0,0 +1,22 @@
+#include <sys/utsname.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+
+int main() {
+
+    struct utsname buf;
+
+    if (uname(&buf) == -1) {
+    	perror("Error_uname");
+    }
+
+    printf("%s\n", buf.sysname);
+    printf("%s\n", buf.nodename);
+    printf("%s\n", buf.release);
+    printf("%s\n", buf.version);
+    printf("%s\n", buf.machine);
+    
+   return 1;
+}
