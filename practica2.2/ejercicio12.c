6021a30a0f0c0e5f374dafa017dae848e9a528b5
JLZ-coder <60971493+JLZ-coder@users.noreply.github.com>
2020-12-05 00:21:27 +0100

    Update ejercicio12.c

--- practica2.2/ejercicio12.c
+++ practica2.2/ejercicio12.c
@@ -1,19 +1,33 @@
-#include <time.h>
-#include <stdio.h>
+#include <unistd.h>
 #include <errno.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
+int main(int argc, char **argv) {
 
-int main() {
+	if (argc > 2 || argc < 2) {
+    	printf("ERROR: Hay que introducir un solo fichero\n");
+    	return 1;
+    }
 
-	time_t *aux;
-	time_t t = time(aux);
+	int fd;
+    fd = open(argv[1], O_CREAT || O_RDWR, 0777);
 
-	if (t == -1) {
-		perror("Error_time");
-	}
-	else {
-		printf("Segundos desde Epoch: %ld \n", t);
-	}
+    if (fd == -1) {
+        perror("Error_open");
+        return 1;
+    }
+
+    if (dup2(fd, 1) == -1) {
+        perror("Error_dup2");
+        return 1;
+    }
+
+    printf("Probando, probando...\n");
+
+    close(fd);
 
     return 0;
 }
