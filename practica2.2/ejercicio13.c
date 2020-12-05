452e5f9281f6a200bc20c9eaff8e98f9b5f8ef38
JLZ-coder <60971493+JLZ-coder@users.noreply.github.com>
2020-12-05 01:20:55 +0100

    Update ejercicio13.c

--- practica2.2/ejercicio13.c
+++ practica2.2/ejercicio13.c
@@ -1,31 +1,49 @@
-#include <time.h>
-#include <sys/time.h>
-#include <stdio.h>
+#include <unistd.h>
 #include <errno.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
+int main(int argc, char **argv) {
 
-int main() {
+	if (argc > 2 || argc < 2) {
+    	printf("ERROR: Hay que introducir un solo fichero\n");
+    	return 1;
+    }
 
-	struct timeval start, end;
-	struct timezone aux;
+	int fd;
+    fd = open(argv[1], O_CREAT | O_RDWR, 0666);
 
-	int i;
+    if (fd == -1) {
+        perror("Error_open");
+        return 1;
+    }
 
-	if (gettimeofday(&start, &aux) == -1) {
-		perror("Error_gettimeofday");
-		return 1;
-	}
+    if (dup2(fd, 1) == -1) {
+        perror("Error_dup2");
+        return 1;
+    }
 
-	for (i = 0; i < 1000000; i++) {}
+    if (dup2(1, 2) == -1) {
+        perror("Error_dup2");
+        return 1;
+    }
 
-	if (gettimeofday(&end, &aux) == -1) {
-		perror("Error_gettimeofday");
-		return 1;
-	}
+    printf("Probando, probando... en salida estandar\n");
 
-	long dif = end.tv_usec - start.tv_usec;
+   	fprintf(stderr, "Probando en salida de error\n");
 
-	printf("Lo que ha tardado la funcion: %ld\n", dif);
+    close(fd);
+
+    //ls > dirlist 2>&1, esto redirecciona la salida estandar a dirlist y luego redirecciona la salida
+    //de error a la salida estandar, la cual ya estaba apuntando a dirlist
+
+    //ls 2>&1 > dirlist, redirecciona la salida de error a la salida estandar, la cual 
+    //escribe en terminal, por lo que la salida de error tambien escribira en terminal.
+    //Si luego redirigimos la salida estandar a dirlist, la salida de error sigue apuntando
+    //a la terminal
+
 
     return 0;
 }
