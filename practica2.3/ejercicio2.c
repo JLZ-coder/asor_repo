b5a42dcad17de07ecc0c7a82ffefd29f543e73a4
JLZ-coder <60971493+JLZ-coder@users.noreply.github.com>
2020-12-16 16:52:40 +0100

    Create ejercicio2.c

---
+++ practica2.3/ejercicio2.c
@@ -0,0 +1,40 @@
+#include <sched.h>
+#include <errno.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+	int pol;
+    pol = sched_getscheduler(0);
+
+    if (pol == -1) {
+        perror("Error_getsched");
+        return 1;
+    }
+
+    int priori_max = sched_get_priority_max(pol);
+    int priori_min = sched_get_priority_min(pol);
+
+    switch (pol) {
+        case SCHED_OTHER:
+            printf("Politica OTHER");
+            break;
+        case SCHED_FIFO:
+            printf("Politica FIFO");
+            break;
+        case SCHED_RR:
+            printf("Politica RR");
+            break;
+        default:
+            printf("Politica Especial");
+    }
+
+    printf(" (%s, %s) : ", priori_min, priori_max);
+
+    struct sched_param param;
+    sched_getparam(0,&param);
+
+   	printf("Prioridad de %i\n", param.sched_priority);
+
+    return 0;
+}
