edce3253043c988d23d2b31b0a1ddfca82079418
JLZ-coder <60971493+JLZ-coder@users.noreply.github.com>
2020-12-20 17:37:16 +0100

    ej7

---
+++ practica2.3/ejercicio7_execvp.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char **argv) {
+
+    if (argc < 2) {
+        printf("ERROR: Hay que introducir un comando\n");
+        return 1;
+    }
+
+    int len = 0;
+    int i;
+
+    for (i = 1; i < argc; i++) {
+        len += strlen(argv[i]) + 1;
+    }
+
+    len += 1;
+
+    char *command = malloc(sizeof(char) * len);
+
+    for (i = 1; i < argc; i++) {
+        strcat(command, argv[i]);
+        strcat(command, " ");
+    }
+
+    int s;
+    s = system(command);
+
+    if (s == -1) {
+        perror("Error_system");
+        return 1;
+    }
+
+    printf("El comando terminó de ejecutarse.\n");
+
+    //La cadena se imprime una vez terminado el comando que se ha pasado a system
+
+    return 0;
+}
