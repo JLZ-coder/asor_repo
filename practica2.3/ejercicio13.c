8229152434708609dbf4105a4b04cd32c16dd2ca
JLZ-coder <60971493+JLZ-coder@users.noreply.github.com>
2020-12-20 21:07:40 +0100

    Create ejercicio13.c

---
+++ practica2.3/ejercicio13.c
@@ -0,0 +1,62 @@
+#include <errno.h>
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+volatile int parado = 0;
+
+void handler(int sig) {
+    if (sig == SIGUSR1) {
+        parado = 1;
+    }
+}
+
+//Como usar sigsuspend cuando hay un temporizador de por medio
+
+int main(int argc, char **argv) {
+
+    if (argc < 2 || argc > 2) {
+        printf("ERROR: Hay que introducir un numero de segundos\n");
+        return 1;
+    }
+
+    int secs = atoi(argv[1]);
+
+    struct sigaction action;
+
+    sigaction(SIGUSR1, NULL, &action);
+    action.sa_handler = handler;
+    sigaction(SIGUSR1, &action, NULL);
+
+    int i = 0; 
+
+    sigset_t signals;
+    sigemptyset(&signals);
+
+    while (i <= secs) {
+
+        printf("Cuenta atras: %i\n", secs - i);
+
+        if (parado == 1) {
+            break;
+        }
+
+        i += 1;
+        sleep(1);
+    }
+
+    printf("\n");
+
+    if (parado == 0) {
+        printf("Borrando el ejecutable... F\n");
+        unlink(argv[0]);
+    }
+    else {
+        printf("Salvado!!!\n");
+    }
+
+    return 0;
+}
