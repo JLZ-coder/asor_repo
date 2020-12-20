f3293ba84290f113301dd1ef04288db91645f726
JLZ-coder <60971493+JLZ-coder@users.noreply.github.com>
2020-12-20 18:54:49 +0100

    Create ejercicio10.c

---
+++ practica2.3/ejercicio10.c
@@ -0,0 +1,2 @@
+//Segun la señal enviada el proceso respondera con un codigo de salida acorde. 
+//Si la señal es para detener el proceso, en la terminal saldra que se ha detenido pero no terminado.
