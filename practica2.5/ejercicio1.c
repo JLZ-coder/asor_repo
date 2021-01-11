6974d0e6061b2b3dd51db2869ffaba224eef90a1
JLZ-coder <60971493+JLZ-coder@users.noreply.github.com>
2021-01-11 10:40:32 +0100

    Update ejercicio1.c

--- practica2.5/ejercicio1.c
+++ practica2.5/ejercicio1.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv) {
 
     memset(&hints, 0, sizeof(struct addrinfo));
     hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
-    hints.ai_socktype = 0; /* Datagram socket */
+    hints.ai_socktype = 0; /* Any socket */
     hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
     hints.ai_protocol = 0;          /* Any protocol */
     hints.ai_canonname = NULL;
